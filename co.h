92505d0cb7db4172b834c87aa2a7a6834e2aa832
Yecheng Fu <cofyc.jackson@gmail.com>
2012-02-07 16:54:25 +0800

    fix

--- co.h
+++ co.h
@@ -17,8 +17,6 @@
 /* local header */
 #include "local.h"
 
-#define CO_DEBUG 1
-
 #if SIZEOF_LONG == 4
 #define MAX_LENGTH_OF_LONG 11
 #elif SIZEOF_LONG == 8
@@ -37,28 +35,19 @@ typedef unsigned char uchar;
 
 /* Compiler */
 extern int coparse(void *compiler_globals);
-
 extern void coerror(const char *str);
 
 /* Wrapper Functions */
+extern void *xmalloc(size_t size);
+extern void *xrealloc(void *ptr, size_t size);
+extern void *xcalloc(size_t nmemb, size_t size);
+extern char *xstrndup(const char *str, size_t len);
 extern bool safe_strtol(const char *str, long int *out);
 
 /* Helper Functions */
 extern void die(const char *err, ...);
-
 extern void usage(const char *err);
-
 extern int error(const char *err, ...);
-
 extern void warning(const char *err, ...);
-
-/* Alloc Functions */
-extern void *xmalloc(size_t size);
-
-extern void *xrealloc(void *ptr, size_t size);
-
-extern void *xcalloc(size_t nmemb, size_t size);
-
-extern char *xstrndup(const char *str, size_t len);
 
 #endif
