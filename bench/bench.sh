e7fef8379eb62faae73a6d9f6b0d164d57aaa3b7
Yecheng Fu <cofyc.jackson@gmail.com>
2012-03-16 18:15:13 +0800

    add bench

---
+++ bench/bench.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# 
+# Bench *.co codes and record results.
+#
+# Author: Yecheng Fu <cofyc.jackson@gmail.com> 
+
+report=results/$(git describe --match "v[0-9]*" HEAD).report
+
+# clear first
+echo -n > $report
+
+# system summary information
+echo "------ system summary ------" >> $report
+uname -a >> $report
+
+# bench
+echo "------ benchmarks -----" >> $report
+for f in $(find . -name "*.co"); do
+    printf ">>> %s\n" $f >> $report
+    cat $f >> $report
+    printf "<<< time\n" >> $report
+    time (../co $f &>/dev/null) 2>> $report
+    echo >> $report
+done
--- vm_opcodes.h
+++ vm_opcodes.h
@@ -29,9 +29,9 @@
 #define OP_TRY                      23
 #define OP_THROW                    24
 #define OP_CATCH                    25
-#define OP_BIND_NAME                26
+#define OP_LOAD_NAME                26
 #define OP_TUPLE_BUILD              27
 #define OP_LIST_BUILD               28
-#define OP_APPEND_ELEMENT              29
+#define OP_APPEND_ELEMENT           29
 
 #endif
