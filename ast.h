ab07646b43f01f5a4e2e8b779664f75a083276fd
Yecheng Fu <cofyc.jackson@gmail.com>
2012-03-31 11:10:58 +0800

    add Conventions

--- ast.h
+++ ast.h
@@ -1,4 +1,31 @@
 #ifndef AST_H
 #define AST_H
+#include "object.h"
+
+enum Node_Type {
+    NODE_IF,
+    NODE_WHILE,
+    NODE_ADD,
+    NODE_SUB,
+    NODE_MUL,
+    NODE_OR,
+    NODE_RETURN,
+};
+
+typedef struct _Node {
+    int type;
+    union {
+        struct _Node *node;
+        COObject *o;
+    } u1;
+    union {
+        struct _Node *node;
+        COObject *o;
+    } u2;
+    union {
+        struct _Node *node;
+        COObject *o;
+    } u3;
+} Node;
 
 #endif
