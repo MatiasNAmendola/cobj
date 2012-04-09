e7c4c557e5ee4309bb56ba931988623e9d6ad20b
Yecheng Fu <cofyc.jackson@gmail.com>
2012-04-09 10:24:44 +0800

    clear

--- bench/bench.sh
+++ bench/bench.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 # 
-# Bench *.co codes and record results.
-#
 # Author: Yecheng Fu <cofyc.jackson@gmail.com> 
-
-
 
 # bench
 for f in $(find . -name "*.co"); do
-    report=results/$f.report
-    # header
-    echo "------ bench $(git describe --match 'v[0-9]*' HEAD) at $(date) ------" >> $report
-    uname -a >> $report
-    echo >> $report
-    printf ">>> %s\n" $f >> $report
-    cat $f >> $report
-    printf "<<< time\n" >> $report
-    time (../co $f &>/dev/null) 2>> $report
-    echo >> $report
+    echo $f
+    time (../co $f &>/dev/null)
 done
